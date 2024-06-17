1fef43c4f40dfa6f59d4a5c704be827a188dbcf9
DownloadableFox <me@downloadablefox.dev>
2024-06-16 19:42:12 -0500

    now prepared to handle commands

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    // Wait for user input
+fn prompt() -> Result<String, std::io::Error> {
     let stdin = io::stdin();
     let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+
+    print!("$ ");
+    io::stdout().flush()?;
+    
+    stdin.read_line(&mut input)?;
+    Ok(input.trim().to_string())
+}
+
+fn handle_command(input: String) {
+    println!("{}: command not found", input);
+}
+
+fn main() {
+    loop {
+        match prompt() {
+            Ok(input) => handle_command(input),
+            Err(e) => println!("lsh: an error occurred reading prompt \"{}\"", e),
+        }
+    }
 }
